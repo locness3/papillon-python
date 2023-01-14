7025f6540d731815e244839b34efe11abf3a446f
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2023-01-14 15:52:58 +0100

    Fix de discussions

--- server.py
+++ server.py
@@ -622,7 +622,7 @@ def discussions(token, response):
                     "id": message.id,
                     "content": message.content,
                     "author": message.author,
-                    "date": message.date.strftime("%Y-%m-%d %H:%M"),
+                    "date": message.date,
                     "seen": message.seen
                 })
 
