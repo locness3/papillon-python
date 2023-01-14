99cf673c885519665042b271bf6f42d0c767eab4
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2023-01-14 15:54:03 +0100

    Update server.py

--- server.py
+++ server.py
@@ -630,7 +630,7 @@ def discussions(token, response):
                 "id": discussion.id,
                 "subject": discussion.subject,
                 "creator": discussion.creator,
-                "date": discussion.date.strftime("%Y-%m-%d %H:%M"),
+                "date": discussion.date,
                 "unread": discussion.unread,
                 "closed": discussion.close,
                 "replyable": discussion.replyable,
