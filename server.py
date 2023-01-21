27a0a02caf3a4ae3fa606f2ed9058fc2dbfe168b
Mizu <40831463+Rexxt@users.noreply.github.com>
2023-01-21 20:55:29 +0100

    Revert ":ambulance: Fixed tester shitting itself and crashing when no response given by API"
    This reverts commit f8cb7626fa1c5b1642aa4a1710f627a573331895.

--- server.py
+++ server.py
@@ -480,7 +480,6 @@ def __get_grade_state(grade_value:str, significant:bool = False) -> int|str :
     """
     
     grade_value = str(grade_value)
-
     if significant:
         grade_translate = [
             "Absent",
--- test.py
+++ test.py
@@ -17,70 +17,46 @@ start = time.time()
 response = requests.get(api_url.rstrip('/') + '/user', params={'token': token})
 end = time.time()
 print(f'/user ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
 
 start = time.time()
 response = requests.get(api_url.rstrip('/') + '/timetable', params={'token': token, 'dateString': '2022-01-05'})
 end = time.time()
 print(f'/timetable ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
 
 start = time.time()
 response = requests.get(api_url.rstrip('/') + '/homework', params={'token': token, 'dateFrom': '2022-01-05', 'dateTo': '2022-01-09'})
 end = time.time()
 print(f'/homework ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
 
 start = time.time()
 response = requests.get(api_url.rstrip('/') + '/grades', params={'token': token})
 end = time.time()
 print(f'/grades ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
 
 start = time.time()
 response = requests.get(api_url.rstrip('/') + '/absences', params={'token': token})
 end = time.time()
 print(f'/absences ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
 
 start = time.time()
 response = requests.get(api_url.rstrip('/') + '/news', params={'token': token})
 end = time.time()
 print(f'/news ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
 
 start = time.time()
 response = requests.get(api_url.rstrip('/') + '/discussions', params={'token': token})
 end = time.time()
 print(f'/discussions ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
 
 start = time.time()
 response = requests.get(api_url.rstrip('/') + '/export/ical', params={'token': token})
 end = time.time()
 print(f'/export/ical ({round((end-start)*1000)}ms):')
-try:
-    print(response.json())
-except:
-    print('fail')
+print(response.json())
